7b398607936ca0d30ed30a0871407f4b7baec516
Peter Goshulak <pgoshulak@gmail.com>
2018-05-31 12:36:01 -0400

    Init server and mongodb

---
+++ server/index.js
@@ -0,0 +1,45 @@
+"use strict";
+
+// Basic express setup:
+
+require('dotenv').config();
+const PORT = process.env.PORT || 8081;
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+const { MongoClient } = require('mongodb');
+const MONGODB_URI = process.env.MONGODB_URI; 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// MongoDB database
+MongoClient.connect(MONGODB_URI, (err, db) => {
+  if (err) {
+    console.error(`Failed to connect: ${MONGODB_URI}`);
+    throw err;
+  }
+  console.log(`Connected to mongodb: ${MONGODB_URI}`);
+
+
+  // The `data-helpers` module provides an interface to the database of tweets.
+  // Because it exports a function that expects the `db` as a parameter, we can
+  // require it and pass the `db` parameter immediately:
+
+  // const DataHelpers = require("./lib/data-helpers.js")(db);
+
+
+  // The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
+  // so it can define routes that use it to interact with the data layer.
+  
+  // const tweetsRoutes = require("./routes/tweets")(DataHelpers);
+
+  // Mount the tweets routes at the "/tweets" path prefix:
+  
+  // app.use("/tweets", tweetsRoutes);
+
+  app.listen(PORT, () => {
+    console.log("Example app listening on port " + PORT);
+  });
+});
