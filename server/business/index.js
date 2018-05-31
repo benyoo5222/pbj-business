cb706aee03af03339a29f7f6679706af8be5d312
Peter Goshulak <pgoshulak@gmail.com>
2018-05-31 19:30:38 -0400

    Refactor server routes to be more DRY

--- server/business/index.js
+++ server/business/index.js
@@ -3,66 +3,50 @@
 const express = require('express');
 const businessRoutes = express.Router();
 
+const returnJson = function(res, err, data) {
+  if (err) {
+    res.status(500).json({ error: err.message })
+  } else {
+    res.json(data)
+  }
+}
+
 module.exports = function(dataHelpers) {
 
   businessRoutes.get('/', (req, res) => {
     // res.json({ message: "get '/' from businessRoutes" })
     dataHelpers.readAllBusinesses((err, data) => {
-      if (err) {
-        res.status(500).json({ error: err.message })
-      } else {
-        res.json(data)
-      }
+      returnJson(res, err, data)
     })
   })
 
   businessRoutes.get('/:id', (req, res) => {
     dataHelpers.readBusiness(req.params.id, (err, data) => {
-      if (err) {
-        res.status(500).json({ error: err.message })
-      } else {
-        res.json(data)
-      }
+      returnJson(res, err, data)
     })
   })
 
   businessRoutes.put('/:id', (req, res) => {
     dataHelpers.updateBusiness(req.params.id, req.body.data, (err, data) => {
-      if (err) {
-        res.status(500).json({ error: err.message })
-      } else {
-        res.json(data)
-      }
+      returnJson(res, err, data)
     })
   })
 
   businessRoutes.get('/:id/services', (req, res) => {
     dataHelpers.readServices(req.params.id, (err, data) => {
-      if (err) {
-        res.status(500).json({ error: err.message })
-      } else {
-        res.json(data)
-      }
+      returnJson(res, err, data)
     })
   })
 
   businessRoutes.put('/:id/services', (req, res) => {
     dataHelpers.updateServices(req.params.id, req.body.data, (err, data) => {
-      if (err) {
-        res.status(500).json({ error: err.message })
-      } else {
-        res.json(data)
-      }
+      returnJson(res, err, data)
     })
   })
 
   businessRoutes.post('/:id/transactions', (req, res) => {
     dataHelpers.createTransaction(req.params.id, req.body.data, (err, data) => {
-      if (err) {
-        res.status(500).json({ error: err.message })
-      } else {
-        res.json(data)
-      }
+      returnJson(res, err, data)
     })
   })
 
