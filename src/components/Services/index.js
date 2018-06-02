1c77cd731b0241a306047b2efbf3b29d91def326
benyoo5222 <benyoo5222@gmail.com>
2018-06-02 17:39:34 +0000

    Able to make AJAX request to edit services

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -3,7 +3,10 @@ import Services from './services.js'
 
 class Serviceslist extends Component {
   render() {
-    const each_service = <Services data= {this.props.data}/>
+    const each_service = this.props.data.services.map(eachService =>
+      <Services data = {eachService} key = {eachService["_id"]}/>
+    )
+
 
     return (
       <main>
