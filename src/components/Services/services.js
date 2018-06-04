ae747af76d904b70556785c68c786ec9e270fa18
benyoo5222 <benyoo5222@gmail.com>
2018-06-04 20:59:11 +0000

    Make an edit to the database using PUT Method with an edit form

--- src/components/Services/services.js
+++ src/components/Services/services.js
@@ -12,55 +12,20 @@ import EditForm from './editserviceform.js'
 
 class Services extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      open: false
-    }
-  }
-
-  editService = (serviceId) => {
-    const array = this.props.data.services.filter( value => value.billingCode !== serviceId)
-    const editedService =
-      {
-        billingCode: `${serviceId}`,
-        description: "See the Future of Jeff!",
-        priceCents: 10000,
-        durationMin: 10
-      }
-    const newestarray = [...array, editedService]
-    newestarray.sort(function(a, b) {
-      return a.billingCode - b.billingCode;
-    })
-
-    axios.put(`http://localhost:5000/api/business/${this.props.data["_id"]}/services`,
-      {data: newestarray})
-      .then(res => this.props.updateService(newestarray))
-      .catch(err => {
-        console.error(err)
-      })
-  }
-
-  handleOpen = () => {
-    this.setState({open: true})
-  }
-
-  handleClose = (close) => {
-    this.setState({open:close})
+  handleOpen = (serviceId) => {
+    this.props.openForm(true, serviceId)
   }
 
   render () {
     return (
       <List>
-        {this.props.data.name === "Ben's Hair salon" &&
-          this.props.data.services.map(eachService =>
-            <ListItem >
+        {this.props.data.services.map(eachService =>
+            <ListItem key={eachService.billingCode}>
               <ListItemText primary= {`${eachService.description} ${eachService.durationMin} ${eachService.priceCents}`} />
 
               <ListItemSecondaryAction>
                 <IconButton aria-label="Comments">
-                  <EditIcon onClick={this.handleOpen}/>
-                  <EditForm open={this.state.open} close={this.handleClose} info={eachService}/>
+                  <EditIcon onClick={this.handleOpen.bind(this, eachService.billingCode)}/>
                 </IconButton>
               </ListItemSecondaryAction>
             </ListItem>
