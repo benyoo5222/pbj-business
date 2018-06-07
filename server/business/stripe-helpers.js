572063522c20231bfe674c0706542bda5ee8b507
benyoo5222 <benyoo5222@gmail.com>
2018-06-07 19:38:23 +0000

    Able to now send email confirmations

--- server/business/stripe-helpers.js
+++ server/business/stripe-helpers.js
@@ -1,8 +1,50 @@
 module.exports = {
   requestStripePayment: function(data) {
     const stripekeys = require('../stripe_secrets.json');
+    const gmailPassword = require('../gmail_password.json')
     const keySecret = stripekeys.keySecret;
     const stripe = require("stripe")(keySecret);
+    const nodemailer = require('nodemailer');
+    const moment = require('moment');
+    const appointmentStart = moment(data.event.start).format('LLL');
+    const appointmentEnd = moment(data.event.end).format('LLL');
+    console.log(data)
+
+  let message = '';
+  if (data.stripeData.token){
+    message = "Thank you for payment. See you then!"
+  } else {
+    message = "See you then!"
+  }
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'benyoo5222@gmail.com',
+      pass: gmailPassword.password
+    }
+  });
+
+  const mailOptions = {
+    from: 'benyoo5222@gmail.com',
+    to: data.customer.email,
+    subject: 'Your appointment details for Ben\'s Hair Salon',
+    text:
+      ` Hi ${data.customer.name}! Thanks for booking an appointment with us.
+
+  This is a summary of your appointment.
+
+  Location: 46 Spadina Ave.
+
+  When: ${appointmentStart} to ${appointmentEnd}.
+
+  The services you have chosen are ${data.services.map(value => value)}.
+
+  The total price is $${data.totalPrice/100}.
+
+  ${message}`
+  }
+
 
 
   stripe.customers.create({
@@ -18,7 +60,13 @@ module.exports = {
     })
   })
   .then(charge => {
-    console.log(charge)
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
   })
     return true
   }
